162eadc9f6ede3f94faaa7c6c9d53eb13a0e9f32
Sukaato <sukaato.dev@proton.me>
2024-03-05 23:35:26 +0100

    ✨ Add `accordion` & `accordion-group` components

---
+++ core/src/components/accordion-group/accordion-group.tsx
@@ -0,0 +1,102 @@
+import { Component, ComponentInterface, Element, Event, EventEmitter, Host, Prop, Watch, h } from '@stencil/core';
+
+@Component({
+  tag: 'pop-accordion-group',
+  shadow: true,
+})
+export class AccordionGroup implements ComponentInterface {
+  @Element() host: HTMLElement;
+
+  /**
+   * If `true`, the user cannot interact with the element.
+   */
+  @Prop({ reflect: true }) readonly?: boolean = false;
+
+  /**
+   * If `true`, the user cannot interact with the element.
+   */
+  @Prop({ reflect: true }) disabled?: boolean = false;
+
+  /**
+   * If `true`, the user can open multiple accordion.
+   */
+  @Prop({ reflect: true }) multiple: boolean = false;
+
+  /**
+   * name of the active the accordion.
+   */
+  @Prop({ mutable: true }) active?: string | string[];
+  @Watch('active')
+  onActiveChange(value: string | string[]) {
+    this.popActiveChange.emit({
+      value,
+    });
+    this.popChange.emit({
+      value,
+    });
+  }
+
+  componentDidLoad(): void {
+    this.#accordions.forEach(accordion => {
+      if (this.readonly) accordion.readonly = this.readonly;
+      if (this.disabled) accordion.disabled = this.disabled;
+    });
+    this.#applyOpen();
+  }
+
+  #applyOpen(): void {
+    if (!this.active) return;
+
+    this.popActiveChange.emit({
+      value: this.active,
+    });
+  }
+
+  get #accordions() {
+    return Array.from(this.host.querySelectorAll('pop-accordion'));
+  }
+
+  /**
+   * The `popChange` event is fired when the user select an open of close an accordion.
+   *
+   */
+  @Event() popChange: EventEmitter<AccordionGroupChangeEventDetail>;
+
+  /**
+   * Emitted when the `active` property has changed.
+   * This is used to ensure that `pop-accordion` can respond
+   * to any value property changes from the group.
+   *
+   * @internal
+   */
+  @Event() popActiveChange!: EventEmitter<AccordionGroupChangeEventDetail>;
+
+  #onClick = (ev: Event) => {
+    ev.preventDefault();
+
+    const accordion = ev.target as HTMLPopAccordionElement;
+    if (!accordion || accordion.disabled || accordion.readonly) return;
+
+    const current = this.active;
+    const newActive = accordion.name;
+
+    if (this.multiple) {
+      this.active =
+        current === undefined
+          ? [newActive]
+          : this.active.includes(newActive)
+            ? [...this.active].filter(a => a !== newActive)
+            : [newActive, ...this.active];
+    } else {
+      this.active = current !== newActive ? newActive : null;
+    }
+  };
+
+  render() {
+    return (
+      <Host onClick={this.#onClick}>
+        <slot />
+      </Host>
+    );
+  }
+}
---
+++ core/src/components/accordion-group/readme.md
@@ -0,0 +1,40 @@
+# pop-accordion-group
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property   | Attribute  | Description                                           | Type                 | Default     |
+| ---------- | ---------- | ----------------------------------------------------- | -------------------- | ----------- |
+| `active`   | `active`   | name of the active the accordion.                     | `string \| string[]` | `undefined` |
+| `disabled` | `disabled` | If `true`, the user cannot interact with the element. | `boolean`            | `false`     |
+| `multiple` | `multiple` | If `true`, the user can open multiple accordion.      | `boolean`            | `false`     |
+| `readonly` | `readonly` | If `true`, the user cannot interact with the element. | `boolean`            | `false`     |
+
+
+## Events
+
+| Event       | Description                                                                        | Type                                           |
+| ----------- | ---------------------------------------------------------------------------------- | ---------------------------------------------- |
+| `popChange` | The `popChange` event is fired when the user select an open of close an accordion. | `CustomEvent<AccordionGroupChangeEventDetail>` |
+
+
+## Dependencies
+
+### Used by
+
+ - [pop-app](../app)
+
+### Graph
+```mermaid
+graph TD;
+  pop-app --> pop-accordion-group
+  style pop-accordion-group fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
