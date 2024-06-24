2a508458b8b871a743185b8f86dea6a6b41287b6
Julien Vaumoron <julien.vaumoron@kanoma.fr>
2024-06-24 15:16:32 +0200

    lint (core): Add prettier organize imports

--- packages/core/src/components/popover/popover.type.ts
+++ packages/core/src/components/popover/popover.type.ts
@@ -1,5 +1,5 @@
 import { TriggerAction } from '#utils/trigger';
-import { Size, ComponentProps, ComponentRef, FrameworkDelegate } from 'src/interface';
+import { ComponentProps, ComponentRef, FrameworkDelegate, Size } from 'src/interface';
 
 export interface PopoverOptions<T extends ComponentRef = ComponentRef> {
   component: T;
