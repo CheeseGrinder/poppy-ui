96287c6240699e73e29bf30d2c15ab37ce79dbb8
Sukaato <sukaato.dev@proton.me>
2025-04-02 21:25:47 +0200

    fix(vue): fix v-model

--- packages/vue/src/plugin.ts
+++ packages/vue/src/plugin.ts
@@ -2,21 +2,7 @@ import type { PoppyUserConfig } from '@poppy-ui/core';
 import { initialize } from '@poppy-ui/core/components';
 import type { Plugin } from 'vue';
 
-const toKebabCase = (eventName: string) => {
-  return eventName.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
-};
-
-const getHelperFunctions = (legacy = false) => {
-  if (legacy) {
-    console.log('REGISTER EVENT IN KEBAB_CASE (popEvent)');
-    return {
-      ael: (el: any, eventName: string, cb: any, opts: any) => el.addEventListener(toKebabCase(eventName), cb, opts),
-      rel: (el: any, eventName: string, cb: any, opts: any) => el.removeEventListener(toKebabCase(eventName), cb, opts),
-      ce: (eventName: string, opts: any) => new CustomEvent(toKebabCase(eventName), opts),
-    };
-  }
-
-  console.log('REGISTER EVENT IN DASH_CASE (pop-event)');
+const getHelperFunctions = () => {
   return {
     ael: (el: any, eventName: string, cb: any, opts: any) => el.addEventListener(eventName, cb, opts),
     rel: (el: any, eventName: string, cb: any, opts: any) => el.removeEventListener(eventName, cb, opts),
@@ -24,7 +10,7 @@ const getHelperFunctions = (legacy = false) => {
   };
 };
 
-export const PoppyVue: Plugin<[(PoppyUserConfig & { legacy?: boolean })?]> = {
+export const PoppyVue: Plugin<[PoppyUserConfig?]> = {
   async install(_, config = {}) {
     /**
      * By default Poppy UI hides elements that
@@ -37,7 +23,7 @@ export const PoppyVue: Plugin<[(PoppyUserConfig & { legacy?: boolean })?]> = {
 
     initialize({
       ...config,
-      helpers: getHelperFunctions(config?.legacy ?? true),
+      helpers: getHelperFunctions(),
     });
   },
 };
