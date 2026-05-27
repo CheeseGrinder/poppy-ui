5e3a9dcff9b8fceba51bbbdcfa48a264e396b346
Sukaato <sukaato.dev@proton.me>
2026-05-27 14:16:22 +0200

    feat: implement List and ListItem components with RouterLink support and layout utilities

---
+++ src/components/display/list/ListItem.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+import type { ListItemProps } from './list-item.props'
+</script>
+
+<script setup lang="ts">
+const props = defineProps<ListItemProps>()
+
+const tag = computed(() => props.to ? RouterLink : 'li')
+</script>
+
+<template>
+  <component
+    :is="tag"
+    class="list-row"
+    :to="to"
+  >
+    <slot />
+  </component>
+</template>
