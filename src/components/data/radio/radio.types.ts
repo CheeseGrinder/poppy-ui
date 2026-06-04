f7ae9f61cf4d42df027651828bf02407153d5d4b
Sukaato <sukaato.dev@proton.me>
2026-06-04 10:00:46 +0200

    feat: add Radio component with configurable properties and documentation

---
+++ src/components/data/radio/radio.types.ts
@@ -0,0 +1,5 @@
+import type { ComponentSize } from '@/types/size.type'
+
+export type RadioColor = 'neutral' | 'primary' | 'secondary' | 'accent' | 'info' | 'success' | 'warning' | 'error'
+
+export type RadioSize = ComponentSize
