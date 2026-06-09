c908123e84e7cc68f7ec59bdaa11e8ebea4825ec
Sukaato <sukaato.dev@proton.me>
2026-06-09 16:38:34 +0200

    feat(stat): #96 add Stat and Stats components with props and context

---
+++ src/components/display/stat/Stats.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { useComponentConfig } from '@/composables/use-component-config'
+import type { ComponentClass } from '@/types/utils.type'
+import { getClass } from '@/utils/build-class.util'
+import { STATS_CONFIG } from './stats.context'
+import type { StatsProps } from './stats.props'
+import type { StatsDirection } from './stats.types'
+
+const statsDirections: ComponentClass<'stats', StatsDirection> = {
+  horizontal: 'stats-horizontal',
+  vertical: 'stats-vertical',
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps<StatsProps>()
+const config = useComponentConfig(STATS_CONFIG, props, {
+  direction: 'horizontal',
+})
+</script>
+
+<template>
+  <div
+    class="stats"
+    :class="getClass(statsDirections, config.direction)"
+  >
+    <slot />
+  </div>
+</template>
---
+++ src/components/display/stat/stats.types.ts
@@ -0,0 +1,1 @@
+export type StatsDirection = 'horizontal' | 'vertical'
